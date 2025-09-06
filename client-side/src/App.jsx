70807d4b78deccdbce1ba4fe7ee9b8df16994ba1
runningpoem30 <aryaanandpathak30@gmail.com>
2025-09-06 15:09:28 +0530

    changes

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,15 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import Auth from './Auth'
+import Dashboard from './pages/Dashboard'
 
-import './App.css'
-import { Routes, Route, Link } from 'react-router-dom';
-import Auth from './Auth';
-function App() {
-
-
+export default function App() {
   return (
-   <Routes>
-    <Route path='/auth' element={<Auth/>}/>
-   </Routes>
+    <Routes>
+      <Route path="/auth/*" element={<Auth />} />
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
   )
 }
-
-export default App
